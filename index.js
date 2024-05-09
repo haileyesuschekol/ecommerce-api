10dc596e995df6e3166749888b27eda31be3ef0c
Haileyesus Chekol <teamdevs7@gmail.com>
2024-05-09 12:27:00 +0100

    callback

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 console.log("before")
-setTimeout(() => {
-  console.log("async .. ")
-}, 2000)
+getUser(1, (user) => {
+  console.log(user)
+  getRepos((uname) => {
+    console.log(uname)
+  })
+})
+
 console.log("after")
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Reading from the database .. ")
+    callback({ id: id, userName: "Mosh" })
+  }, 2000)
+  return id
+}
+
+function getRepos(username) {
+  setTimeout(() => {
+    console.log("calling from api ...")
+    username(["repo1", "repo2", "repo3"])
+  }, 2000)
+}
