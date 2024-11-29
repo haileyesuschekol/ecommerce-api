2a1eb0c2127dc44156dce76abc8b8f901a1b26d7
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-29 23:12:28 +0000

    validate User email using validator package

--- model/User.js
+++ model/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const validator = require("validator")
 //define user model
 const userSchema = new mongoose.Schema({
   name: {
@@ -10,6 +11,10 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provid email!"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email!",
+    },
   },
   passsword: {
     type: String,
