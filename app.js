e3de9c685b29203bac360afa4ad1a14a11ae9558
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-29 21:52:38 +0000

    use errorhandler middlewares and morgan

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 require("dotenv").config()
+require("express-async-errors")
+
+//express
 const express = require("express")
 const app = express()
 
+//rest middlewares
+const morgan = require("morgan")
+
 //import Db connection
 const connectDb = require("./db/connect")
+
+//error handler middlewares
+const errorHandlerMiddleware = require("./middleware/error-handler")
+const notFoundMiddleware = require("./middleware/not-found")
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("E-COMMERCE API")
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 //define port
 const port = process.env.Port || 8080
