15a17597637152f90fb4ea43a502bf35b108b04a
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-29 21:26:02 +0000

    Connect to database and  server.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+//import Db connection
+const connectDb = require("./db/connect")
+
+//define port
+const port = process.env.Port || 8080
+const start = () => {
+  connectDb()
+  app.listen(port, console.log(`server listen to port ${port} ...`))
+}
+
+//start server and Database
+start()
--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,11 @@
-const mongoose = require('mongoose');
+require("dotenv").config()
+const mongoose = require("mongoose")
 
-const connectDB = (url) => {
-  return mongoose.connect(url);
-};
+//Database connection
+const connectDB = () => {
+  return mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("successfully connected to Db"))
+}
 
-module.exports = connectDB;
+module.exports = connectDB
