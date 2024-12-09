d30d0e5c36c7ed166de832122751eeb4a925b6cc
Haileyesus Chekol <teamdevs7@gmail.com>
2024-12-09 01:41:26 +0000

    add some security packages

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const app = express()
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
+const rateLimiter = require("express-rate-limit")
+const helmet = require("helmet")
+const xss = require("xss-clean")
+const mongoSanitize = require("express-mongo-sanitize")
 //routes
 const authRoute = require("./routes/authRouter")
 const userRoute = require("./routes/userRouter")
@@ -22,7 +26,19 @@ const connectDb = require("./db/connect")
 const errorHandlerMiddleware = require("./middleware/error-handler")
 const notFoundMiddleware = require("./middleware/not-found")
 
-app.use(morgan("tiny"))
+app.set("trust proxy", 1)
+app.use(
+  rateLimiter({
+    windwMs: 15 * 60 * 1000,
+    max: 60,
+  })
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
+
+// app.use(morgan("tiny"))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(express.static("./public"))
